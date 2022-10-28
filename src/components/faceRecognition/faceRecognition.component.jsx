6ae96ac111464b0c5522a13c2455acdc031f4cee
timDeHof <this.is.tim.dehof@gmail.com>
2022-10-28 17:43:56 -0400

    created a face recognition component

--- src/components/faceRecognition/faceRecognition.component.jsx
+++ src/components/faceRecognition/faceRecognition.component.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import "./faceRecognition.styles.css";
 
-const FaceRecognition = () => {
-  return <div>faceRecognition.component</div>;
+const FaceRecognition = ({ imageUrl, box }) => {
+  const { leftCol, topRow, rightCol, bottomRow } = box;
+  return (
+    <div className='center ma'>
+      <div className='absolute mt2'>
+        <img
+          id='inputimage'
+          src={imageUrl}
+          alt=''
+          width='500px'
+          height='auto'
+        />
+        <div
+          className='bounding-box'
+          style={{
+            top: topRow,
+            right: rightCol,
+            bottom: bottomRow,
+            left: leftCol,
+          }}></div>
+      </div>
+    </div>
+  );
 };
 
 export default FaceRecognition;
