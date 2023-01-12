2971d58427ce4700e70ad886a2d7ee27aa5cc955
timDeHof <this.is.tim.dehof@gmail.com>
2023-01-12 16:44:53 -0500

    fixing the styling of the logo

--- src/components/logo/logo.component.jsx
+++ src/components/logo/logo.component.jsx
@@ -7,7 +7,7 @@ const Logo = () => {
   return (
     <div className='ma4 mt0'>
       <Tilt className='Tilt br2 shadow-2' options={{ max: 55 }} style={{ height: 150, width: 150 }}>
-        <div className='Tilt-inner pa3'>
+        <div className='Tilt-inner pa3 center'>
           <img style={{ paddingTop: "5px" }} alt='brain logo' src={brain} />
         </div>
       </Tilt>
